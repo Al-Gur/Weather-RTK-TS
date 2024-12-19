a0afc5b79504bfa215479fefe52826d25694edc9
Alexander <a@a>
2024-12-19 11:27:52 +0300

    AsyncThunk

--- src/features/api/weatherAction.ts
+++ src/features/api/weatherAction.ts
@@ -1,25 +1,19 @@
-import {setWeather} from "../weatherSlice.ts";
-import {setMessage} from "../messageSlice.ts";
 import {api_key, base_url} from "../../utils/constants.js";
-import {AppDispatch} from "../../app/store.ts";
+import {createAsyncThunk} from "@reduxjs/toolkit";
 
-export const getWeather = (city: string) => {
-    return (dispatch: AppDispatch) => {
-        fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`)
-            .then(result => result.json())
-            .then(data => {
-                dispatch(setWeather({
+export const getWeather = (city: string) => createAsyncThunk(
+    'fetch/weather',
+    async (city) => {
+        const response = await fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`);
+        const data = await response.json();
+        return {
                     country: data.sys.country,
                     city: data.name,
                     temp: data.main.temp,
                     pressure: data.main.pressure,
                     sunset: new Date(data.sys.sunset * 1000)
-                }))
-                dispatch(setMessage(''))
-            })
-            .catch(e => {
-                console.log(e);
-                dispatch(setMessage('Enter correct city name'))
-            })
-    }
-}
+                }
+
+     }
+);
+
--- src/features/weatherSlice.ts
+++ src/features/weatherSlice.ts
@@ -1,12 +1,27 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {getWeather} from "./api/weatherAction.ts";
+import {setMessage} from "./messageSlice.ts";
+import {useAppDispatch} from "../app/hooks.ts";
 
+const dispatch = useAppDispatch();
 const weatherSlice = createSlice({
     name: "weather",
     initialState: {},
-    reducers: {
-        setWeather: (state, action) => action.payload
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(getWeather.pending, () => dispatch(setMessage('Pending...'))
+            .addCase(getWeather.fulfilled,
+                        (_state, action) => {
+                           dispatch(setMessage(''));
+                           return action.payload
+                        })
+            .addCase(getWeather.rejected,
+                        e => {
+                            console.log(e);
+                            dispatch(setMessage('Enter correct city name'))
+                        })
     }
 })
 
-export const { setWeather } = weatherSlice.actions;
 export default weatherSlice.reducer;
